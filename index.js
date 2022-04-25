999b34d08e554709f1f2a3b6f103bad11f62e6ac
jesse zenon <jesse.zenon@gmail.com>
2022-04-25 15:06:37 -0500

    commit tree 22c8ae310b1563ea0d78a59580c61a631ab19f1f

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-// code your solution here
+// // code your solution here
+
+// function saturdayFun(activity = "roller-skate") {
+//   return `This Saturday, I want to ${activity}!`;
+// }
+
+// let mondayWork = function (activity = "go to the office") {
+//   return `This Monday, I will ${activity}.`;
+// };
+
+// function wrapAdjective(adj = "*") {
+//   return function (word = "special") {
+//     return `You are ${adj}${word}${adj}!`;
+//   };
+// }
 
 function saturdayFun(activity = "roller-skate") {
   return `This Saturday, I want to ${activity}!`;
 }
 
-let mondayWork = function (activity = "go to the office") {
+function mondayWork(activity = "go to the office") {
   return `This Monday, I will ${activity}.`;
-};
+}
 
 function wrapAdjective(adj = "*") {
-  return function (word = "special") {
-    return `You are ${adj}${word}${adj}!`;
+  return function (par = "special") {
+    return `You are ${adj}${par}${adj}!`;
   };
 }
